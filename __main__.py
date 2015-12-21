2f33e18bef498d297d9fd1c5c1e023567e1e39c5
Samuel P. Gillispie II <spgill@vt.edu>
2015-12-21 01:13:11 -0500

    Got a nice preliminary GUI ready
    Now I just have to add the underlying tech to trace the transformations

--- __main__.py
+++ __main__.py
@@ -2,35 +2,53 @@
 import argparse
 
 # local module imports
+import machine_gui
 import machine as machine
 
 
+def run_cli(args):
+    """Run the Enigma Machine with the command-line interface"""
+    print(args.input)
+    em = machine.Machine(None, args.rotors, args.reflector)
+    print(em.transcodeString(args.input))
+
+
+def run_gui(args):
+    """Run the Enigma Machine using the Graphical User Interface"""
+    machine_gui.main()
+
+
+# Define the master parser
 parser = argparse.ArgumentParser(
     description='Process some data through a simulated Enigma machine'
 )
+subparsers = parser.add_subparsers()
 
-parser.add_argument(
+# Sub-parser for the command-line interface
+parser_cli = subparsers.add_parser('cli')
+parser_cli.add_argument(
     '--rotors', '-ro',
     nargs='+',
     type=str,
     required=True
 )
-
-parser.add_argument(
+parser_cli.add_argument(
     '--reflector', '-rf',
     type=str,
     required=True
 )
-
-parser.add_argument(
+parser_cli.add_argument(
     'input',
     type=str
 )
+parser_cli.set_defaults(func=run_cli)
+
+# Sub-parser for the graphical user interface
+parser_gui = subparsers.add_parser('gui')
+parser_gui.set_defaults(func=run_gui)
 
 
 args = parser.parse_args()
 print()
-print(args)
-
-m = machine.Machine(None, args.rotors, args.reflector)
-print(m.transcodeString(args.input))
+print('ARGS', args)
+args.func(args)
---
+++ machine_gui.py
@@ -0,0 +1,242 @@
+# stdlib imports
+import math
+import time
+import tkinter
+import tkinter.ttk
+
+# local module imports
+import chassis
+import machine
+import rotors
+
+
+class MachineGUI(tkinter.Tk):
+    """A Tkinter window creating a graphical simulation of an Enigma Machine"""
+
+    def __init__(self, *args, **kwargs):
+        """Overwrite the builtin init method"""
+        super().__init__(*args, **kwargs)
+
+        # Let's get this party started
+        self._config()
+        self._setup()
+        self.mainloop()
+
+    def _config(self):
+        """Configure the properties and constants of the gui"""
+        # window properties
+        self.title('Enigma Machine GUI')
+
+        # constants
+        self.CANVAS_ROTOR_SIZE = 256
+        self.CANVAS_ROTOR_RADI = self.CANVAS_ROTOR_SIZE / 2
+        self.CANVAS_ROTOR_PAD = 8
+
+    def _setup(self):
+        """Setup all the GUI elements"""
+
+        # Collect a list of rotor names
+        self.rotors_classes = rotors._RotorBase.__subclasses__()
+        self.rotors_names = ['']
+        self.rotors_lookup = {}
+        for rotor in self.rotors_classes:
+            if rotor == rotors._ReflectorBase:
+                continue
+            self.rotors_names.append(rotor._name)
+            self.rotors_lookup[rotor._name] = rotor._short
+        self.rotors_names.sort()
+
+        # Collect a list of reflector names
+        self.reflector_classes = rotors._ReflectorBase.__subclasses__()
+        self.reflector_names = ['']
+        self.reflector_lookup = {}
+        for reflector in self.reflector_classes:
+            self.reflector_names.append(reflector._name)
+            self.reflector_lookup[reflector._name] = reflector._short
+
+        # Create all the selection boxes for the different rotors and such
+        self.rotors_options = [
+            tkinter.StringVar(), tkinter.StringVar(), tkinter.StringVar()
+        ]
+        for var in self.rotors_options:
+            var.set(self.rotors_names[1])
+        for i in range(3):
+            tkinter.ttk.OptionMenu(self, self.rotors_options[i], *self.rotors_names)\
+                .grid(column=i, row=0)
+        self.reflector_option = tkinter.StringVar()
+        self.reflector_option.set(self.reflector_names[1])
+        tkinter.ttk.OptionMenu(self, self.reflector_option, *self.reflector_names)\
+            .grid(column=3, row=0)
+
+        # Get the canvas ready to go
+        self.canvas = tkinter.Canvas(
+            self,
+            width=(self.CANVAS_ROTOR_SIZE * 4),
+            height=self.CANVAS_ROTOR_SIZE
+        )
+        self.canvas.grid(column=0, row=1, columnspan=4)
+        self.canvas_redraw()
+
+        # Add the entry widgets for input and output
+        self.var_input = tkinter.StringVar()
+        tkinter.ttk.Label(self, text='Input:')\
+            .grid(column=0, row=2, sticky='E')
+        self.entry_input = tkinter.ttk.Entry(self, textvariable=self.var_input)
+        self.entry_input.grid(column=1, row=2, columnspan=2, sticky='WE')
+
+        self.var_output = tkinter.StringVar()
+        tkinter.ttk.Label(self, text='Output:')\
+            .grid(column=0, row=3, sticky='E')
+        tkinter.ttk.Entry(self, textvariable=self.var_output, state='readonly')\
+            .grid(column=1, row=3, columnspan=2, sticky='WE')
+
+        # oh, add a button too
+        self.button_start = tkinter.ttk.Button(
+            self,
+            text='START',
+            command=self.start
+        )
+        self.button_start.grid(column=3, row=2, rowspan=2, sticky='WESN')
+
+        # Configure the grid
+        for x in range(4):
+            self.grid_columnconfigure(x, weight=1)
+
+    def canvas_redraw(self):
+        """Clear the canvas and draw the basic shapes"""
+        self.rotors = [
+            {'inner': [], 'outer': []},
+            {'inner': [], 'outer': []},
+            {'inner': [], 'outer': []},
+            {'inner': [], 'outer': []}
+        ]
+
+        # Loop for three rotors and a reflector
+        for i in range(4):
+
+            # Calculate a few useful coordinates
+            topleft = (tlx, tly) = (
+                self.CANVAS_ROTOR_SIZE * i,
+                0
+            )
+            bottomright = (brx, bry) = (
+                self.CANVAS_ROTOR_SIZE * (i + 1),
+                self.CANVAS_ROTOR_SIZE
+            )
+            center = (centerx, centery) = (
+                self.CANVAS_ROTOR_SIZE / 2 + self.CANVAS_ROTOR_SIZE * i,
+                self.CANVAS_ROTOR_SIZE / 2
+            )
+            # self.canvas.create_oval(topleft, bottomright)
+
+            # Loop through the alphabet
+            for j in range(26):
+                letter = rotors._RotorBase._abet[j]
+                # Do some trig B)
+                angle = (2.0 * math.pi) / 26 * j - math.pi / 2
+                anglex = math.cos(angle)
+                angley = math.sin(angle)
+
+                # outer circle
+                self.canvas.create_oval(
+                    (tlx + 2, tly + 2),
+                    (brx - 2, bry - 2)
+                )
+
+                # outer pips
+                self.canvas.create_oval(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 6) - 3,
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 6) - 3
+                    ),
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 6) + 3,
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 6) + 3
+                    )
+                )
+                self.rotors[i]['outer'].append(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 6),
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 6)
+                    )
+                )
+
+                # outer letters
+                self.canvas.create_text(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 16),
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 16)
+                    ),
+                    text=letter
+                )
+
+                # middle circle
+                self.canvas.create_oval(
+                    (tlx + 24, tly + 24),
+                    (brx - 24, bry - 24),
+                    dash=[2, 2]
+                )
+
+                # inner letters
+                self.canvas.create_text(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 32),
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 32)
+                    ),
+                    text=letter
+                )
+
+                # inner pips
+                self.canvas.create_oval(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 42) - 3,
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 42) - 3
+                    ),
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 42) + 3,
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 42) + 3
+                    )
+                )
+                self.rotors[i]['inner'].append(
+                    (
+                        centerx + anglex * (self.CANVAS_ROTOR_RADI - 42),
+                        centery + angley * (self.CANVAS_ROTOR_RADI - 42)
+                    )
+                )
+
+                # inner circle
+                self.canvas.create_oval(
+                    (tlx + 46, tly + 46),
+                    (brx - 46, bry - 46)
+                )
+
+    def start(self):
+        """Start the ciphering process"""
+        # disable input
+        self.button_start['state'] = 'disabled'
+        self.entry_input['state'] = 'disabled'
+        self.var_output.set('')
+
+        # resolve the rotors and reflector
+        rotors = [self.rotors_lookup[o.get()] for o in self.rotors_options]
+        reflector = self.reflector_lookup[self.reflector_option.get()]
+
+        # initialize the machine
+        m = machine.Machine(None, rotors, reflector)
+
+        # slowly feed the data through
+        for char in self.var_input.get():
+            result = m.transcodeString(char)
+            self.var_output.set(self.var_output.get() + result)
+            self.update()
+            time.sleep(0.25)
+
+        # re-enabled input
+        self.button_start['state'] = 'normal'
+        self.entry_input['state'] = 'normal'
+
+
+def main():
+    """Main method for the gui. Run this to initialize a new instance"""
+    chassis.init()
+    MachineGUI()
