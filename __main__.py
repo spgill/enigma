d3205ed86a68ce75eaef281fc547786138829368
Samuel P. Gillispie II <spgill@vt.edu>
2016-06-27 20:02:04 -0400

    Completely removed the GUI

--- __main__.py
+++ __main__.py
@@ -3,11 +3,9 @@ import argparse
 import bz2
 import datetime
 import io
-import re
 import sys
 
 # local module imports
-import machine_gui
 import enigma.machine
 import rotors
 
@@ -27,9 +25,207 @@ def _serialize_plugboard(stack):
             stack[y] = None
     return pairs
 
+def main():
+    """Main method (seems kinda redundant in the main file but w/e)"""
+    # Define the master parser
+    parser = argparse.ArgumentParser(
+        description='Process some data through a simulated Enigma machine'
+    )
 
-def run_cli(args):
-    """Run the Enigma Machine with the command-line interface"""
+    # Rotor args
+    parser.add_argument(
+        '--plugboard', '-p',
+        nargs='+',
+        default=[],
+        type=str,
+        required=False,
+        help="""
+        Specify a list of character pairings for the plugboard.
+        ex; AB CF HJ
+        """
+    )
+    parser.add_argument(
+        '--rotors', '-ro',
+        nargs='+',
+        type=str,
+        required=False,
+        help="""
+        Specify a list of rotors in the following format:
+        SHORTNAME[:SETTING[:NOTCHES]]
+        ex; com1:C:QV
+        """
+    )
+    parser.add_argument(
+        '--reflector', '-rf',
+        type=str,
+        required=False
+    )
+
+    # State args
+    parser.add_argument(
+        '--state-path', '-s',
+        type=str,
+        default='',
+        required=False,
+        help="""
+        Path for the state file (loading or creating).
+        Can be used in lieu of manually specifying rotors and reflectors.
+        """
+    )
+    parser.add_argument(
+        '--state-create', '-sc',
+        action='store_true',
+        required=False,
+        help='Take the rotor and reflector args and save it to the state file.'
+    )
+    parser.add_argument(
+        '--state-update', '-su',
+        action='store_true',
+        required=False,
+        help="""
+        After processing, save the changed rotor state back to the state file.
+        This allows for a continuous rotor progression over multiple
+        script invocations. THERE IS NO ROLLBACK, SO BACK IT UP.
+        """
+    )
+    parser.add_argument(
+        '--state-print', '-sp',
+        action='store_true',
+        required=False,
+        help="""
+        Print the state information to stdout and exit.
+        """
+    )
+    parser.add_argument(
+        '--state-seed', '-ss',
+        type=str,
+        default='',
+        required=False,
+        help="""
+        String seed for a randomly generated state.
+        """
+    )
+
+    # Input args
+    parser.add_argument(
+        '--input', '-i',
+        type=str,
+        default='',
+        required=False,
+        help="""
+        Input a string via this command line argument.
+        """
+    )
+    parser.add_argument(
+        '--input-std', '-is',
+        action='store_true',
+        required=False,
+        help="""
+        Read data from stdin pipe.
+        """
+    )
+    parser.add_argument(
+        '--input-path', '-ip',
+        type=str,
+        default='',
+        required=False,
+        help="""
+        Open and read data from file path.
+        """
+    )
+    parser.add_argument(
+        '--input-bz2', '-iz',
+        action='store_true',
+        required=False,
+        help="""
+        Run input through BZ2 decompression before processing.
+        """
+    )
+
+    # Output args
+    parser.add_argument(
+        '--output-std', '-os',
+        action='store_true',
+        required=False,
+        help="""
+        Write output to the stdout pipe.
+        """
+    )
+    parser.add_argument(
+        '--output-path', '-op',
+        type=str,
+        required=False,
+        help="""
+        Write output to the specified file path.
+        """
+    )
+    parser.add_argument(
+        '--output-bz2', '-oz',
+        action='store_true',
+        required=False,
+        help="""
+        Run output through BZ2 compression before writing.
+        """
+    )
+
+    # Other arguments
+    parser.add_argument(
+        '--chunk-size', '-c',
+        type=int,
+        default=128,
+        required=False,
+        help="""
+        Chunk size for reading and writing data.
+        """
+    )
+    parser.add_argument(
+        '--benchmark', '-b',
+        action='store_true',
+        required=False,
+        help="""
+        Benchmark the processing time (prints results to stderr).
+        """
+    )
+    parser.add_argument(
+        '--no-progress', '-np',
+        action='store_true',
+        required=False,
+        help="""
+        Suppress the progress meter that is normal written to stderr.
+        """
+    )
+    parser.add_argument(
+        '--sanitize', '-sa',
+        action='store_true',
+        required=False,
+        help="""
+        Sanitize input to resemble typical enigma text (alpha, caps, and no space).
+        """
+    )
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        required=False,
+        help="""
+        Enable verbosity; printing LOTS of messages to stderr.
+        """
+    )
+    parser.add_argument(
+        '--typewriter', '-t',
+        action='store_true',
+        required=False,
+        help="""
+        Enable typewriter mode. Press a key, and the translated key is written
+        to the console; similar to an actual enigma machine. Not particularly
+        useful, just really cool to play with. (currently Windows only)
+        """
+    )
+
+    if len(sys.argv) == 1:
+        print('("--help" flag inferred from no args)\n')
+        sys.argv.append('--help')
+    args = parser.parse_args()
+
     # Initialize the enigma machine using specified rotors or a state file
     machine = None
     if args.state_path and not args.state_create:
@@ -182,226 +378,6 @@ def run_cli(args):
     if args.state_update:
         if args.state_path:
             open(args.state_path, 'wb').write(machine.stateGet())
-
-
-def run_gui(args):
-    """Run the Enigma Machine using the Graphical User Interface"""
-    print('GUI is currently non-functional. Exiting...')
-    exit()
-    machine_gui.main()
-
-
-def main():
-    """Main method (seems kinda redundant in the main file bu w/e)"""
-    # Define the master parser
-    parser = argparse.ArgumentParser(
-        description='Process some data through a simulated Enigma machine'
-    )
-    subparsers = parser.add_subparsers()
-
-    # Sub-parser for the command-line interface
-    parser_cli = subparsers.add_parser('cli')
-
-    # Rotor args
-    parser_cli.add_argument(
-        '--plugboard', '-p',
-        nargs='+',
-        default=[],
-        type=str,
-        required=False,
-        help="""
-        Specify a list of character pairings for the plugboard.
-        ex; AB CF HJ
-        """
-    )
-    parser_cli.add_argument(
-        '--rotors', '-ro',
-        nargs='+',
-        type=str,
-        required=False,
-        help="""
-        Specify a list of rotors in the following format:
-        SHORTNAME[:SETTING[:NOTCHES]]
-        ex; com1:C:QV
-        """
-    )
-    parser_cli.add_argument(
-        '--reflector', '-rf',
-        type=str,
-        required=False
-    )
-
-    # State args
-    parser_cli.add_argument(
-        '--state-path', '-s',
-        type=str,
-        default='',
-        required=False,
-        help="""
-        Path for the state file (loading or creating).
-        Can be used in lieu of manually specifying rotors and reflectors.
-        """
-    )
-    parser_cli.add_argument(
-        '--state-create', '-sc',
-        action='store_true',
-        required=False,
-        help='Take the rotor and reflector args and save it to the state file.'
-    )
-    parser_cli.add_argument(
-        '--state-update', '-su',
-        action='store_true',
-        required=False,
-        help="""
-        After processing, save the changed rotor state back to the state file.
-        This allows for a continuous rotor progression over multiple
-        script invocations. THERE IS NO ROLLBACK, SO BACK IT UP.
-        """
-    )
-    parser_cli.add_argument(
-        '--state-print', '-sp',
-        action='store_true',
-        required=False,
-        help="""
-        Print the state information to stdout and exit.
-        """
-    )
-    parser_cli.add_argument(
-        '--state-seed', '-ss',
-        type=str,
-        default='',
-        required=False,
-        help="""
-        String seed for a randomly generated state.
-        """
-    )
-
-    # Input args
-    parser_cli.add_argument(
-        '--input', '-i',
-        type=str,
-        default='',
-        required=False,
-        help="""
-        Input a string via this command line argument.
-        """
-    )
-    parser_cli.add_argument(
-        '--input-std', '-is',
-        action='store_true',
-        required=False,
-        help="""
-        Read data from stdin pipe.
-        """
-    )
-    parser_cli.add_argument(
-        '--input-path', '-ip',
-        type=str,
-        default='',
-        required=False,
-        help="""
-        Open and read data from file path.
-        """
-    )
-    parser_cli.add_argument(
-        '--input-bz2', '-iz',
-        action='store_true',
-        required=False,
-        help="""
-        Run input through BZ2 decompression before processing.
-        """
-    )
-
-    # Output args
-    parser_cli.add_argument(
-        '--output-std', '-os',
-        action='store_true',
-        required=False,
-        help="""
-        Write output to the stdout pipe.
-        """
-    )
-    parser_cli.add_argument(
-        '--output-path', '-op',
-        type=str,
-        required=False,
-        help="""
-        Write output to the specified file path.
-        """
-    )
-    parser_cli.add_argument(
-        '--output-bz2', '-oz',
-        action='store_true',
-        required=False,
-        help="""
-        Run output through BZ2 compression before writing.
-        """
-    )
-
-    # Other arguments
-    parser_cli.add_argument(
-        '--chunk-size', '-c',
-        type=int,
-        default=128,
-        required=False,
-        help="""
-        Chunk size for reading and writing data.
-        """
-    )
-    parser_cli.add_argument(
-        '--benchmark', '-b',
-        action='store_true',
-        required=False,
-        help="""
-        Benchmark the processing time (prints results to stderr).
-        """
-    )
-    parser_cli.add_argument(
-        '--no-progress', '-np',
-        action='store_true',
-        required=False,
-        help="""
-        Suppress the progress meter that is normal written to stderr.
-        """
-    )
-    parser_cli.add_argument(
-        '--sanitize', '-sa',
-        action='store_true',
-        required=False,
-        help="""
-        Sanitize input to resemble typical enigma text (alpha, caps, and no space).
-        """
-    )
-    parser_cli.add_argument(
-        '--verbose', '-v',
-        action='store_true',
-        required=False,
-        help="""
-        Enable verbosity; printing LOTS of messages to stderr.
-        """
-    )
-    parser_cli.add_argument(
-        '--typewriter', '-t',
-        action='store_true',
-        required=False,
-        help="""
-        Enable typewriter mode. Press a key, and the translated key is written
-        to the console; similar to an actual enigma machine. Not particularly
-        useful, just really cool to play with. (currently Windows only)
-        """
-    )
-
-    parser_cli.set_defaults(func=run_cli)
-
-    # Sub-parser for the graphical user interface
-    parser_gui = subparsers.add_parser('gui')
-    parser_gui.set_defaults(func=run_gui)
-
-    if len(sys.argv) == 1:
-        print('("--help" flag inferred from no args)\n')
-        sys.argv.append('--help')
-    args = parser.parse_args()
-    args.func(args)
 
 # Run if main
 if __name__ == '__main__':
